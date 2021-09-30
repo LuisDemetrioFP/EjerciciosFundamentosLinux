7ab4ab6483ce5cda8a0443df04e9732646f89370
LuisDemetrioFP <82707467+LuisDemetrioFP@users.noreply.github.com>
2021-09-30 19:48:41 +0200

    Add files via upload

---
+++ EjerciciosFundamentosLinux/EjerciciosFundamentosLinux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variables para definir texto de entrada y definir variable de texto por defecto
+
+DEFAULT=$1
+
+# Crea las carpetas recursivamente y con permisos para creador y grupo del usuario del EJERCICIO_01
+
+mkdir -p -m755 foo/{empty,dummy}/
+
+# Crea los archivos file1.txt y file2.txt dentro de la carpeta foo/dummy del EJERCICIO_01
+
+touch foo/dummy/file{1,2}.txt/
+
+# Inserta el texto 'Me encanta la bash' o el texto que hemos indicado como parametro en la ejecucion del script en el archivo file1.txt del EJERCICIO_01
+
+echo ${DEFAULT:-'Que me gusta el bash!!'} > foo/dummy/file1.txt
+
+# Vuelca el contenido de foo/dummy/file1.txt a foo/dummy/file2.txt y mueve file2.txt a la carpeta foo/empty
+
+cat foo/dummy/file1.txt > foo/dummy/file2.txt | mv foo/dummy/file2.txt foo/empty
+
+exit
