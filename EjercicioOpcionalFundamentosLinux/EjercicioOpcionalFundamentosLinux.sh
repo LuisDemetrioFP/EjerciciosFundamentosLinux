1968c714c5d73e89cb89fa296c56cfa64a1f9135
LuisDemetrioFP <82707467+LuisDemetrioFP@users.noreply.github.com>
2021-09-30 19:50:15 +0200

    Add files via upload

---
+++ EjercicioOpcionalFundamentosLinux/EjercicioOpcionalFundamentosLinux.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Mensajes de error si los argumentos se quedan vacios:
+
+echo ${1:?'Introduce una web entre " " como primer parametro'} ${2:?'SUGERENCIA: introduce una palabra entre " " para buscar en la web como segundo parametro'}
+
+#Variable para contar que el parametro 1 no este vacio (tengo que practicar mas con boleanasen bash :P)
+
+count_var1=$(echo $1 | wc -w)
+
+#bucle while para ejecutar los comandos solo si se han introducido argumentos:
+
+while [ $count_var1 -ge 0 ]
+do
+
+#Creamos archivo donde volcar los datos:
+
+	touch fileweb.txt
+
+#Variable para descargar y volcar los datos:	
+
+	newfile=$(curl -s $1 > fileweb.txt)
+
+#Variable de busqueda y conteo de linea:
+
+	findword=$(grep -ion $2 fileweb.txt)
+
+#Muestra en pantalla el resultado de la busqueda y el numero de linea:	
+
+	echo 'La palabra buscada en el texto esta en la linea:' $findword
+	exit
+done
+
+
+
